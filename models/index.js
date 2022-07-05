233a32f78403eb209457630a6db607d98cd56ef6
saharkichi <saharkichi@gmail.com>
2022-07-05 19:28:31 -0400

    startingAssociations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: ''
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ''
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey: ''
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: ''
+});
 
 module.exports = {
   Product,
