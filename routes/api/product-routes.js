270b634f03468f15e8f4f6c61e5e782636ee4327
saharkichi <saharkichi@gmail.com>
2022-07-05 20:39:48 -0400

    productRoutes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model: Category}, {model: Tag, through: ProductTag}]
+  }).then(products => res.status(200).json(products))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag, through: ProductTag}]
+  }).then(product => res.status(200).json(product))
 });
 
 // create new product
@@ -91,6 +97,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+  }
+})
+  .then(deletedProduct => res.status(200).json(deletedProduct))
+  .catch(err => res.json(err));
 });
 
 module.exports = router;
