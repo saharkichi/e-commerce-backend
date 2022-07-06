318f31269d6864fc90e727fc4dc82785ee867ecb
saharkichi <saharkichi@gmail.com>
2022-07-05 20:01:22 -0400

    SyncSequelize

--- models/index.js
+++ models/index.js
@@ -5,14 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: ''
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: ''
-});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, {
